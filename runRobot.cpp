2b96b9686bf15d421e839878ed4fc0a49166ac13
BudgiePanic <bsanson90@gmail.com>
2019-05-19 14:49:59 +1200

    updating to master copy

---
+++ runRobot.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include "E101.h"
+#include <unistd.h>
+#include <time.h>
+
+//run robot needs fields for
+//current quadrant
+//camera position
+
+//rubRobot needs to create new 
+//camera object
+//network object
+//wheel objects
+
+//main method
+
+class Camera{
+	//fields here
+	public:
+	//methods here
+	
+	}
+	
+class GateOpen{
+	//fields here
+	public:
+	//methods here
+	void openGate(){
+			//code here...
+			cout>>"Opening the gate...">>endl;
+		}
+	
+	}
+	
+	
+	
+class Wheels{
+	//fields here
+	public:
+	//methods here
+	
+	}
+
+class RunRobot{
+	//fields here
+	public:
+	//methods here
+	
+	}
+
+
+
+int main(){
+	//example code
+		//make a new object
+	GateOpen open;
+		//call a method on the object
+	open.openGate();
+	return 0;
+	}
