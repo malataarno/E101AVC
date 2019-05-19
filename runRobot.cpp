1b5fea790b054167e97bcde92947f2f5d4545bab
BudgiePanic <bsanson90@gmail.com>
2019-05-19 16:02:34 +1200

    started making gate open class
    To do:
    -test the server address
    -find the port number
    -find the message
    -deal with server response

--- runRobot.cpp
+++ runRobot.cpp
@@ -23,12 +23,36 @@ class Camera{
 	
 class GateOpen{
 	//fields here
+	private:	
+	char[15] serverAddress = { '1','2','7','.','0','0','1','.','0','0','1','.','0','0','1' };	//char server_addr[15] = { '1','2','7','.','0','0','1','.','0','0','1','.','0','0','1' }; TEST THE ADDRESS
+	int port = 0;																				//need to find what the port number is
+	char[24] message = { '' };																	//find what the message should be
+
 	public:
 	//methods here
 	void openGate(){
-			//code here...
-			cout>>"Opening the gate...">>endl;
+		//connect to the server
+		int isConnected = 0;
+		while (isConnected == 0) {
+			isConnected = connect_to_server(serverAddress,port);
 		}
+		
+		//send message to server
+		send_to_server(message);
+			
+		int msg_recieved = 0;
+		while (msg_recieved == 0) { 
+			//wait for response, returns 0 when false???
+			msg_recieved = receive_from_server();
+		}
+		
+		//where does response go???
+			//create password
+
+		send_to_server(password);
+		//gate opens, mission accomplished
+
+	};
 	
 	}
 	
