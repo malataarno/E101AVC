bb56296953b7d8031afa56510dd423a2480694d5
gaskinjare <50566443+gaskinjare@users.noreply.github.com>
2019-05-20 17:20:23 +1200

    Update runRobot.cpp

--- runRobot.cpp
+++ runRobot.cpp
@@ -16,10 +16,44 @@
 
 class Camera{
 	//fields here
+	int speedDiff;
+ double errorRate=0;
+ double finalError=0;
+int countc=0;
+int main(){
+int i;
+int pix [320];
+init(0);
+
 	public:
-	//methods here
-	
-	}
+	void doCamera{
+	open_screen_stream();
+while(true){
+         hardware_exchange();take_picture(); update_screen(); int countc=0;
+while(countc <320)
+{pix [countc] = (int)get_pixel(120,countc,3);
+ countc++;}
+
+int max=0;
+int min=10000000;
+ for(i=0;i<320;i++){ if(pix[i]>max){max=pix[i];}}
+ for(i=0;i<320;i++){if(pix[i]<min){min=pix[i];}}
+
+ int midValue=(max+min)/2;
+ for(i=0;i<320;i++){if(pix[i]>midValue){pix[i]=0;}
+ else{pix[i]=1;}}
+void getError(){
+errorRate=0;
+finalError=0;
+for(i=0;i<320;i++){
+errorRate=(i-160)*pix[i];
+finalError=finalError+errorRate;}
+	return finalError;}
+close_screen_stream();
+return 0;
+}
+}
+}
 	
 class GateOpen{
 	//fields here
@@ -60,8 +94,16 @@ class GateOpen{
 	
 class Wheels{
 	//fields here
+	double finalError
 	public:
+	void setSpeed(){
 	//methods here
+	camera a;
+	finalError=a.getError;
+	speedDiff=(finalError/1000);
+set_motors(1,48+speedDiff);
+set_motors(5,48-speedDiff);}
+}
 	
 	}
 
@@ -80,5 +122,9 @@ int main(){
 	GateOpen open;
 		//call a method on the object
 	open.openGate();
+	
+	camera on;
+	on.runCamera();
+	
 	return 0;
 	}
