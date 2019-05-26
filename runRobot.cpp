b449b36f599189656ec09544ea25facdee5347bf
BudgiePanic <bsanson90@gmail.com>
2019-05-26 18:04:01 +1200

    reformatted code & made methods
    Reformatted the code on camera class to make it easier to read, didnt change any of the actual code.
    
    Added comments on potential errors.
    
    coded some methods in wheel class.

--- runRobot.cpp
+++ runRobot.cpp
@@ -14,53 +14,71 @@
 
 //main method
 
-class Camera{
+class Camera{		//Ben: reformatted code to make it easier to read - didn't change any code
 	//fields here
 	private:
 	int speedDiff;
  	double errorRate=0;
  	double finalError=0;
 	int countc=0;
-	int main(){ //main???
+	//main(){ //main???
 	int i;
 	int pix [320];
 	init(0);
 
-public:
-void doCamera{
-	open_screen_stream();
-	while(true){
-         hardware_exchange();take_picture(); update_screen(); int countc=0;
-	while(countc <320)
-	{pix [countc] = (int)get_pixel(120,countc,3);
- 	countc++;}
+	public:
+	void doCamera{	//what is this method suppossed to do?
+		open_screen_stream();
+		while (true) {	//this loop never exits, runs forever.
+			hardware_exchange();
+			take_picture();
+			update_screen();
+			int countc = 0;
 
-	int max=0;
-	int min=10000000;
- 	for(i=0;i<320;i++){ if(pix[i]>max){max=pix[i];}}
- 	for(i=0;i<320;i++){if(pix[i]<min){min=pix[i];}}
+			while (countc < 320) {
+				pix[countc] = (int)get_pixel(120, countc, 3);
+				countc++;
+			}
 
- 	int midValue=(max+min)/2;
- 	for(i=0;i<320;i++){if(pix[i]>midValue){pix[i]=0;}
- 	else{pix[i]=1;}
+			int max = 0;
+			int min = 10000000;
+			for (i = 0; i < 320; i++) { if (pix[i] > max) { max = pix[i]; } }
+			for (i = 0; i < 320; i++) { if (pix[i] < min) { min = pix[i]; } }
+
+			int midValue = (max + min) / 2;
+			for (i = 0; i < 320; i++) {
+				if (pix[i] > midValue) {
+					pix[i] = 0;
+				}
+				else {
+					pix[i] = 1;
+				}
+
+			}
+
+		}
+		//close_screen_stream();
 	};
 		
 
-double getError(){ //should probably return an Int
-	errorRate=0;
-	finalError=0;
-	for(i=0;i<320;i++){
-	errorRate=(i-160)*pix[i];
-	finalError=finalError+errorRate;}
-	return finalError;
-	}; //once you call return the method will exit. -- calling return on a void method, changed to double
+	double getError(){ //should probably return an Int
+	
+		errorRate=0;
+		finalError=0;
+		for(i=0;i<320;i++){
+			errorRate=(i-160)*pix[i];
+			finalError=finalError+errorRate;
+		}
 		close_screen_stream();
+		return finalError;
+	}; //once you call return the method will exit. -- calling return on a void method, changed to double
+		//close_screen_stream();
 	
 		//return 0;
 	
-}
-}
-}
+};
+//}
+//}
 	
 class GateOpen{
 	//fields here
@@ -94,35 +112,57 @@ class Wheels{
 	//fields here
 	private:
 	double finalError;
+	//shouldnt speedDiff be declared here?
 	public:
 	//methods here
 	void setSpeed(){
 		
 		camera a;
-		finalError=a.getError;
+		finalError=a.getError();			
 		speedDiff=(finalError/1000);
 		set_motors(1,48+speedDiff);
 		set_motors(5,48-speedDiff);}
+		//hardware exchange method is never called
+		//update_hardware();
 	};
 	
-	void reverse(){
-	//reverse the robot
-		//call when the line is 'lost'
-	//call the get error method
-		//if line is found, set the motors to neurtral and exit
-		//if no, set the motor speed to reverse and check again
-	};
 	
 	void stop(){
-	//set the motor speed to stop the robot
+		//set the motor speed to stop the robot
+		set_motors(1, 48);
+		set_motors(5, 48);
+		update_hardware();
 	};
+
+	/*
+		Reverses the robot, stops reversing when get error method returns non-zero value.
+	*/
+	void reverse(){
+		//call when the line is 'lost'
+		camera a;
+		//setting motors to reverse
+		set_motors(1,48-3);
+		set_motors(5,48+3);
+		update_hardware();
+		while (true) {
+			double error = a.getError();
+			if (error != 0) { //line regained
+				break;
+			}
+			sleep1(10); //sleep 10 ms
+		}
+		this.stop();
+	};
+	
+
 	
 	};
 
 class RunRobot{
 	//fields here
-	bool cameraPosition; //true if facing ground
-	int currentQuadrant;
+	private:
+	bool cameraPosition = true; //true if facing ground
+	int currentQuadrant = 1; //area 1 is the gate, initial position
 	public:
 	//methods here
 	void changeCameraPos(){
@@ -135,13 +175,23 @@ class RunRobot{
 		cameraPosition = !cameraPosition;
 	};
 	
-	void followLine(){
+	int followLine(){
 		//the method for quadrant one
 		//follow the curvy line on the ground (for how long? testing needed)
+		//returns 1 when section is complete
+		return 2;
 	};
 	
+	int navMaze() {
+		//the method for quadrant one
+		//follow the curvy line on the ground (for how long? testing needed)
+		//returns 1 when section is complete
+		return 3;
+	};
+
 	void startRobot(){
 		//the first method to be called, opens the gate and calls follow line
+		
 	};
 	    //what does the robot need to do in each area?
 		//if in quadrant 0
@@ -167,7 +217,7 @@ int main(){
 	open.openGate();
 	
 	camera on;
-	on.runCamera();
+	on.runCamera();	//this method does not exist
 	
 	return 0;
 	}
